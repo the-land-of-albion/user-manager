57c3127add7d36ea77cbda6b32bad7852c58cb82
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2021-03-07 14:30:46 +0100

    added level command

---
+++ src/commands/profile/level.ts
@@ -0,0 +1,34 @@
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+import {fetch} from "../../config/fetch"
+import { dialect } from "../../speech";
+import { Args } from "../../speech/speech.interface";
+import config from "../../config";
+
+const args: Args[] = [{id: "bio", type: "string", default: "", prompt: true}]
+class GetLvl extends Command {
+  constructor() {
+    super("lvl", {
+      aliases: ["lvl", "level", "rank"],
+      category: "profile",
+      description: "Get a user's level/rank.",
+    });
+  }
+
+  async exec(message: Message, args: Record<string, any>) {
+    fetch(`${config.api.prefix}/user/${message.member?.id}`, "GET")
+      .then((res) => {
+        if (!res.ok) {
+          return message.reply("🚨 Unfortunately, I can't see you in our database.");
+        }
+        return res.json();
+      })
+      .then((data) => {
+
+        return message.reply(` Lvl: ${Math.floor(data.experience / 10)}`);
+      })
+
+  }
+}
+
+export default GetLvl;
